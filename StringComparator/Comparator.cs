45f79c43f80bc4fda79ca8a43c41e62cb8a64d91
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2022-04-22 21:01:39 -0300

    Adicionar arquivos de projeto.

---
+++ StringComparator/Comparator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace StringComparator
+{
+    public class Comparator
+    {
+        public static decimal compare(string Original, string Target)
+        {
+            char[] OriginalArray = new char[Original.Length];
+            char[] TargetArray = new char[Target.Length];
+            int letter = 0;
+            decimal correct = 0;
+            foreach (char letters in OriginalArray)
+            {
+                OriginalArray[letter] = letters;
+                letter++;
+            }
+            letter = 0;
+            foreach (char letters in TargetArray)
+            {
+                TargetArray[letter] = letters;
+                letter++;
+            }
+
+            for (int i = 0; i < Original.Length; i++)
+            {
+                try
+                {
+                    if (OriginalArray[i] == TargetArray[i])
+                    {
+                        correct++;
+                    }
+                }
+                catch
+                {
+                    break;
+                }
+            }
+            decimal porcentagem = (correct * 100 / Original.Length);
+            porcentagem = Math.Round(porcentagem, 2);
+            return porcentagem;
+        }
+    }
+}
